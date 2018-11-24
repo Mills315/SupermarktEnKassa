418a62c3850cbc41c58915ffce90971c093cbde8
Yoran <yoran315@gmail.com>
2018-11-24 13:29:44 +0100

    4th

--- src/Supermarkt/Main.java
+++ src/Supermarkt/Main.java
@@ -1,22 +1,34 @@
 package Supermarkt;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 
         Artikel luiers = new Luiers(10);
+        Artikel robijn = new Robijn(3);
+        Artikel brinta = new Brinta(15);
+        Artikel chinesegroenten = new ChineseGroenten(6);
+        Artikel kwark = new Kwark(7);
+
 
         System.out.println(((Luiers) luiers).getAantal());
         System.out.println(((Luiers) luiers).getPrijs());
 
+        // als xx is instanceof Artikel then afrekenmethode?
+    }
 
+    private static List<Artikel> getArtikel(){
+        List<Artikel> artikelen = new ArrayList<>();
+        artikelen.add(new Luiers(10));
 
-
-
-
+        return artikelen;
 
 
     }
+
 
 
 }
