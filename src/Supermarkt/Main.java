e9b5181a7c3d211bf2a0a2df8b9ed2e649d5c81f
Yoran <yoran315@gmail.com>
2018-11-24 15:56:51 +0100

    8, wisselgeld calculator zit er in met teruggave per munt/briefsoort

--- src/Supermarkt/Main.java
+++ src/Supermarkt/Main.java
@@ -8,11 +8,16 @@ public class Main {
     public static void main(String[] args) {
 
         List<Artikel> artikelen = getArtikel();
-        printPrijsArtikelen(getArtikel());
-
+        //  printPrijsArtikelen(getArtikel());
+        System.out.println("------+");
         System.out.println(totaalPrijs(artikelen));
+        betalen(555.55, artikelen);
 
+        // bedenk manieren om kortingen te verwerken, bijvoorbeeld met modulo en een randomInt/weekdag voor weekdagen/korting
     }
+
+
+    // hier worden artikelen toegevoegd, met aantallen
 
     private static List<Artikel> getArtikel() {
         List<Artikel> artikelen = new ArrayList<>();
@@ -24,11 +29,15 @@ public class Main {
         return artikelen;
     }
 
+    // hier worden de afzonderlijke totaalprijzen per product geprint
+
     private static void printPrijsArtikelen(List<Artikel> artikelen) {
         for (Artikel artikel : artikelen) {
             System.out.println(artikel.getPrijs());
         }
     }
+
+    // hier worden de totaalprijs van de hele 'winkelwagen' geprint
 
     private static double totaalPrijs(List<Artikel> artikelen) {
         double totaal = 0.0;
@@ -36,5 +45,67 @@ public class Main {
             totaal = totaal + artikel.getPrijs();
         }
         return totaal;
+    }
+
+    // hier wordt er 'geld' gegeven; als dat meer is dan het totaal, wordt er wisselgeld berekend per 100/10/1/0.1/0.01-tal
+
+    private static void betalen(double geld, List<Artikel> artikelen) {
+        double wisselgeld;
+        double totaal = 0.0;
+        for (Artikel artikel : artikelen) {
+            totaal = totaal + artikel.getPrijs();
+        }
+        if (geld >= totaal) {
+            int b100 = 0;
+            int b10 = 0;
+            int b1 = 0;
+            int b010 = 0;
+            int b001 = 0;
+            wisselgeld = (geld - totaal);
+
+            // wisselgeld heeft meer dan 2 cijfers achter de komma?? heel vreemd.. even vragen aan Gillis/Hans
+
+            System.out.println(wisselgeld);
+
+            // aantal divisions met modulo ? dit werkt ook.
+            while (wisselgeld > 100) {
+                wisselgeld -= 100;
+                b100++;
+            }
+            while (wisselgeld > 10) {
+                wisselgeld -= 10;
+                b10++;
+            }
+            while (wisselgeld > 1) {
+                wisselgeld -= 1;
+                b1++;
+            }
+            while (wisselgeld > 0.1) {
+                wisselgeld -= 0.1;
+                b010++;
+            }
+            while (wisselgeld > 0.01) {
+                wisselgeld -= 0.01;
+                b001++;
+            }
+
+            // dit stukje staat erin om het meer dan 2 cijfers achter de komma te ondervangen. had er niet in hoeven staan. of misschien toch na de kortingen zooi, kom ik zo wel achter
+
+            while (wisselgeld > 0.001) {
+                wisselgeld = wisselgeld - wisselgeld;
+            }
+            if (wisselgeld == 0.0) {
+                System.out.println("Hier heeft u:");
+                System.out.println(b100 + " briefjes van 100");
+                System.out.println(b10 + " briefjes van 10");
+                System.out.println(b1 + " munten van 1");
+                System.out.println(b010 + " munten van 10 cent");
+                System.out.println(b001 + " munten van 1 cent");
+            }
+            if (geld < totaal) {
+                System.out.println("Dat is niet genoeg om de rekening te betalen!");
+            }
+        }
+
     }
 }
